a5909ba68e37661c55b3138490ad75505fda9ab5
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-04-27 20:31:39 -0300

    First commit.

---
+++ src/test/java/com/thyago/MinhaLoja/MinhaLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thyago.MinhaLoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
